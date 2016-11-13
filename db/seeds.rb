18957d67c1e1b70ed626348bde46805ff073bd08
Jordan Hudgens <jordan@wow.com>
2016-11-12 19:17:55 -0600

    038_generating_document_data_rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create_table "freelance_documents", force: :cascade do |t|
+#     t.string   "title"
+#     t.string   "description"
+#     t.text     "file_url"
+#     t.text     "image_url"
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
+#   end
+
+10.times do |d|
+	FreelanceDocument.create!(
+		title: "Document #{d}",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+		file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+		image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+	)
+end
